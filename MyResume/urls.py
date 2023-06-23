41c606cd9a154d98da4537bdee35059da502e0c4
aish199820 <122533555+aish199820@users.noreply.github.com>
2023-06-23 13:12:22 +0530

    Update urls.py

--- MyResume/urls.py
+++ MyResume/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('',views.home_view, name='home'),
     path('skill/',views.skills_view,name="skills"),
-    path('contact/',views.contact_view,name="contact"),
+    # path('contact/',views.contact_view,name="contact"),
     path('education/',views.eduction_view,name="education"),
     path('course/',views.courses_view,name="courses"),
     path('experience/',views.experience_view,name="experiences"),
